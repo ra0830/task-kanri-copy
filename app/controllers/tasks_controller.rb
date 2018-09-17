c5c0e47dcdcc2c0a5ec9b08ffadf5e1b6a9acae9
asanorintaro <asanorintaro19880830@gmail.com>
2018-09-17 16:45:48 +0900

    ステップ１７まで

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,22 +2,22 @@ class TasksController < ApplicationController
    before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-
-    @tasks = Task.all.reverse_order
-    #@tasks = Task.page(params[:page])
+    # @tasks = Task.
+    @tasks = Task.all.reverse_order.page(params[:page])
+    #@tasks = Task.paginate(page:params[:page], per_page: 5 )
 
   #   # 並び替え
     if params[:sample].present? || params[:sample] == 'true'
-      @tasks = Task.order(:period)
+      @tasks = Task.order(:period).page(params[:page])
     elsif params[:fast].present? || params[:fast] == 'true'
-      @tasks = Task.order(:priority)
+      @tasks = Task.order(:priority).page(params[:page])
     else
-      @tasks = Task.all.reverse_order
+      @tasks = Task.all.reverse_order.page(params[:page])
       #@tasks = Task.page(params[:page])
     end
     #タイトル検索
     if params[:search].present?
-      @tasks = Task.search(params[:search])
+      @tasks = Task.search(params[:search], params[:page])
     end
     #状態検索
     # if params[:task].present? && params[:task][:search_status] == 'true'
@@ -27,10 +27,9 @@ class TasksController < ApplicationController
     #   @tasks = Task.all.reverse_order
     # end
     #状態検索（form_with使用、モデルに分散）
-    if params[:search_status].present?
-      @tasks = Task.search_status(params[:search_status])
+    if params[:task].present? && params[:task][:search_status].present?
+      @tasks = Task.search_status(params[:task][:search_status], params[:task][:status], params[:page])
     end
-
   end
 
   def new
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,20 +1,20 @@
 class Task < ApplicationRecord
-
   validates :title, presence: true
 
-  def self.search(search)
+
+  def self.search(search, pages)
     if search
-      Task.where(['title LIKE ?', "%#{search}%"])
+      Task.where(['title LIKE ?', "%#{search}%"]).page(pages)
     else
-      Task.all.reverse_order
+      Task.all.reverse_order.page(pages)
     end
   end
 
-  def self.search_status(search_status)
+  def self.search_status(search_status, task_status, pages)
     if search_status == 'true'
-      Task.where(status: params[:task][:status])
+      Task.where(status: task_status).page(pages)
     else
-      Task.all.reverse_order
+      Task.all.reverse_order.page(pages)
     end
   end
 
