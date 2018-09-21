51ab593c5de04521da6a7b99c076a931e60a9672
asanorintaro <asanorintaro19880830@gmail.com>
2018-09-21 14:33:26 +0900

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
+  validates :name,  presence: true, uniqueness: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }          
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :tasks
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
